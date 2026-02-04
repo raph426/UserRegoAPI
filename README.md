9d615af29c4643c8bde8227b435e736e25e34fa3
Raphael Dakanay <raphael.dakanay@gmail.com>
2026-02-04 20:25:20 +1300

    Added test, fixed jwt crypto issue

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # UserRegoAPI
+
+This is a user registration API programmed and built in Rust.
+
+## features
+- registers users with username and password
+- generate JSONwebtokens for authentication
+- uses Tokio crate for asynchronous capabilities
+- Routes using Axum Router
+
+## project structure
+```graphsql
+src/
+|-main.rs     # entrypoint
+|-models.rs   # define data structures
+|-handlers.rs #
+|-state.rs
+|-auth.rs
+```
+
+## how to build
+
+### prerequisites
+- rustc and cargo 1.93.0
+- (optional) `curl` to test API
+
+### clone repo
+```bash
+git clone https://github.com/raph426/UserRegoAPI.git
+cd user UserRegoAPI
+```
+
+### build and run
+
+```bash
+cargo build
+cargo run
+```
+
+### test
+
+```bash
+cargo test
+```
+
+## usage
+
+### registering a new user ( w/ curl )
+
+```bash
+curl -X POST http://127.0.0.1:3000/register -H "Content-Type: application/json" -d '{"username": "reinhard", "pwd": "kircheis488"}'
+```
+
+expected response:
+
+```sh
+{"token": <jwt_token_here>}
+```
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// import modules from src/
 mod auth;
 mod handlers;
 mod models;
@@ -7,22 +8,58 @@ use axum::{Router, routing::post};
 use state::AppState;
 use tower_http::trace::TraceLayer;
 
+// MAIN FN
 #[tokio::main]
 async fn main() {
     //initialise shared appstate
     let state = AppState::new("secret-key".to_string());
 
-    //build router
+    //build axum router
     let app = Router::new()
-        .route("/register", post(handlers::register))
+        .route("/register", post(handlers::register)) // mapped to POST /register to register fn in handler module
         .with_state(state) // attach shared appstate to all routes
         .layer(TraceLayer::new_for_http()); // logger
 
-    // bind tcp socket
+    // bind tcp socket to localhost
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
 
     // start the http server
     axum::serve(listener, app).await.unwrap();
+}
+
+// test api
+#[cfg(test)]
+mod tests {
+    use axum::{body::Body, http::Request};
+    use tokio_test::block_on;
+    use tower::ServiceExt;
+
+    // use modules in 'this'
+    use crate::{handlers, state::AppState};
+    use axum::{Router, routing::post};
+
+    #[test]
+    fn register_user_success() {
+        block_on(async {
+            let state = AppState::new("test-secret".to_string());
+
+            let app = Router::new()
+                .route("/register", post(handlers::register))
+                .with_state(state);
+
+            let request = Request::builder()
+                .method("POST")
+                .uri("/register")
+                .header("content-type", "application/json")
+                .body(Body::from(r#"{"username":"reinhard","pwd":"kircheis"}"#))
+                .unwrap();
+
+            let response = app.oneshot(request).await.unwrap();
+
+            // checks if response is == to 200 "success"
+            assert_eq!(response.status(), 200);
+        });
+    }
 }
